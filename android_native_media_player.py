78879cae700b33e487e7ded0df2a065d77abef1a
Wasim Afser <wasim.afser@gmail.com>
2020-05-10 16:40:32 +0530

    change native media player to SoundPLayer Implementation

--- android_native_media_player.py
+++ android_native_media_player.py
@@ -11,12 +11,26 @@ class SoundAndroidPlayer(Sound):
 
     def __init__(self, **kwargs):
         self._mediaplayer = None
+        self.quitted = False
         self._state = ''
         self.state = 'stop'
         super(SoundAndroidPlayer, self).__init__(**kwargs)
 
+    def __del__(self):
+        self.unload()
+
+    def _player_callback(self, selector, value):
+        if self._mediaplayer is None:
+            return
+        if selector == 'quit':
+            def close(*args):
+                self.quitted = True
+                self.unload()
+            Clock.schedule_once(close, 0)
+        elif selector == 'eof':
+            Clock.schedule_once(self._do_eos, 0)
+
     def load(self):
-        print("LOADING FROM NATIVE ANDROID PLAYER")
         self.unload()
         self._mediaplayer = MediaPlayer()
         self._mediaplayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
@@ -25,23 +39,25 @@ class SoundAndroidPlayer(Sound):
         self._state = 'paused'
 
     def unload(self):
-        self.stop()
-        self._mediaplayer = None
+        if self._mediaplayer:
+            self._mediaplayer = None
         self._state = ''
         self.state = 'stop'
+        self.quitted = False
 
     def play(self):
-        if not self._mediaplayer:
+        if self._state == 'playing':
+            super(SoundAndroidPlayer, self).play()
             return
+        if not self._mediaplayer:
+            self.load()
+        self._mediaplayer.start()
         self._state = 'playing'
         self.state = 'play'
-        self._mediaplayer.start()
         super(SoundAndroidPlayer, self).play()
 
     def stop(self):
-        if not self._mediaplayer:
-            return
-        elif self._state == 'playing':
+        if self._mediaplayer and self._state == 'playing':
             # self._mediaplayer.reset()
             self._mediaplayer.pause()
             self._state = 'paused'
@@ -49,24 +65,30 @@ class SoundAndroidPlayer(Sound):
         super(SoundAndroidPlayer, self).stop()
 
     def seek(self, position):
-        print(position)
-        if not self._mediaplayer:
+        if self._mediaplayer is None:
             return
-        self._mediaplayer.seekTo(float(position/1000), 2)
+        self._mediaplayer.seekTo(float(position*1000.))
 
     def get_pos(self):
-        if self._mediaplayer:
+        if self._mediaplayer is not None:
             return self._mediaplayer.getCurrentPosition() / 1000.
-        return super(SoundAndroidPlayer, self).get_pos()
+        # return super(SoundAndroidPlayer, self).get_pos()
+        return 0
 
     def on_volume(self, instance, volume):
-        if self._mediaplayer:
+        if self._mediaplayer is not None:
             volume = float(volume)
             self._mediaplayer.setVolume(volume, volume)
 
     def _get_length(self):
-        if self._mediaplayer:
+        if self._mediaplayer is not None:
             return self._mediaplayer.getDuration() / 1000.
         return super(SoundAndroidPlayer, self)._get_length()
+
+    def _do_eos(self, *args):
+        if not self.loop:
+            self.stop()
+        else:
+            self.seek(0.)
 
 SoundLoader.register(SoundAndroidPlayer)
