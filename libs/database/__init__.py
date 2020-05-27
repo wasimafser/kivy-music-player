91da51f9bc5f4cbb54ef5dff674b92c1d7ece7ec
Wasim Afser <wasim.afser@gmail.com>
2020-05-27 19:46:07 +0530

    use the dict_factory instead of sqlite3.ROW

--- libs/database/__init__.py
+++ libs/database/__init__.py
@@ -1,4 +1,11 @@
 import sqlite3
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx,col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 con = sqlite3.connect('libs/database/matrix.db')
-con.row_factory = sqlite3.Row
+# con.row_factory = sqlite3.Row
+con.row_factory = dict_factory
